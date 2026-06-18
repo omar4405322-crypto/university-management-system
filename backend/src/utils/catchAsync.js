7772678d8ddfe0e4a55da6a2a33065c26461645e
Omar Mokhtar <omar4405322@gmail.com>
2026-06-18 06:34:44 +0300

    convert utils/catchAsync.js to TypeScript with export= for CJS interop

--- backend/src/utils/catchAsync.js
+++
@@ -1,9 +0,0 @@
-/**
- * Wraps async functions to catch errors and pass them to the global error handler
- * @param {Function} fn - Async function to wrap
- */
-module.exports = fn => {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next);
-  };
-};
