34a51a0e85afadcd8871a66239a11e1fcc523eef
Omar Mokhtar <omar4405322@gmail.com>
2026-08-19 03:50:24 +0300

    chore(api-server): remove obsolete ad-hoc test and dangerous debug scripts

--- artifacts/api-server/getEmails.cjs
+++
@@ -1,3 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-prisma.user.findMany({take:5}).then(users => console.log(users.map(u => u.email))).finally(() => prisma.$disconnect());
--- artifacts/api-server/run_migration.cjs
+++
@@ -1,4 +0,0 @@
-const { spawn } = require('child_process');
-const cp = spawn('npx.cmd', ['prisma', 'migrate', 'dev', '--name', 'remove_session_type'], { stdio: ['pipe', 'inherit', 'inherit'] });
-cp.stdin.write('y\n');
-cp.stdin.end();
